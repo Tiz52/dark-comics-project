b9199649ab32de6e94901ada807a0945ca188db3
carlos <carls.mrz@gmail.com>
2022-06-01 23:47:01 -0500

    md problem

--- pages/checkout/_middleware.ts
+++ pages/checkout/_middleware.ts
@@ -6,19 +6,13 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const session = await getToken({req, secret: process.env.NEXTAUTH_SECRET});
 
   if (!session) {
-    const {origin, pathname} = req.nextUrl.clone();
-    return NextResponse.redirect(`${origin}/auth/login?p=${pathname}`);
+    // const {origin, pathname} = req.nextUrl.clone();
+    // return NextResponse.redirect(`${origin}/auth/login?p=${pathname}`);
+    const url = req.nextUrl.clone();
+    url.pathname = "/auth/login";
+    url.search = `p=${req.page.name}`;
+    return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
-
-  // const {token = ""} = req.cookies;
-
-  // try {
-  //   // await jwt.isValidToken(token);
-  //   return NextResponse.next();
-  // } catch (error) {
-  //   const requestedPage = req.page.name;
-  //   return NextResponse.redirect(`/auth/login?p=${requestedPage}`);
-  // }
 }
