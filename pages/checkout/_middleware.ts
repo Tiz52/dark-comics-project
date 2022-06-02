da3c5e5f39b4b24a5593579e7feb2cc607d930a2
carlos <carls.mrz@gmail.com>
2022-06-02 03:03:52 -0500

    next

--- pages/checkout/_middleware.ts
+++ pages/checkout/_middleware.ts
@@ -6,12 +6,12 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const session = await getToken({req, secret: process.env.NEXTAUTH_SECRET});
 
   if (!session) {
-    // const {origin, pathname} = req.nextUrl.clone();
-    // return NextResponse.redirect(`${origin}/auth/login?p=${pathname}`);
-    const url = req.nextUrl.clone();
-    url.pathname = "/auth/login";
-    url.search = `p=${req.page.name}`;
-    return NextResponse.redirect(url);
+    const {origin, pathname} = req.nextUrl.clone();
+    return NextResponse.redirect(`${origin}/auth/login?p=${pathname}`);
+    // const url = req.nextUrl.clone();
+    // url.pathname = "/auth/login";
+    // url.search = `p=${req.page.name}`;
+    // return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
