5901bed448273d233bda6c2adc6dfd91f1f58aa3
carlos <carls.mrz@gmail.com>
2022-06-01 22:40:34 -0500

    final

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   images: {
-    domains: ["upload.wikimedia.org","res.cloudinary.com"],
+    domains: ["upload.wikimedia.org", "res.cloudinary.com"],
   },
 };
 
